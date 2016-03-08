92cea03ba8dd1f777770b1514966ddcd1d31c245
Eric Peterson <epeterson320@gmail.com>
2016-03-08 13:29:55 -0500

    Add test dependencies

---
+++ torchFlashlight/src/androidTest/java/co/ericp/flashlight/FlashlightImplTest.java
@@ -0,0 +1,5 @@
+package co.ericp.flashlight;
+
+public class FlashlightImplTest {
+    
+}
---
+++ torchFlashlight/src/main/java/co/ericp/flashlight/FlashlightImpl.java
@@ -0,0 +1,17 @@
+package co.ericp.flashlight;
+
+import co.ericp.flashlight.base.IFlashlight;
+
+class FlashlightImpl implements IFlashlight {
+    public void turnOn() {
+
+    }
+
+    public void turnOff() {
+
+    }
+
+    public boolean isOn() {
+        throw new UnsupportedOperationException("To do");
+    }
+}
